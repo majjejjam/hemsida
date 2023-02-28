87c470ee92f9f5c4efcfa61f62b94a9fd1274dc8
Miró Allard <miro.allard@pm.me>
2023-02-28 22:14:38 +0100

    Began work on basic string replacement

--- pollen.rkt
+++ pollen.rkt
@@ -1,10 +1,22 @@
 #lang racket/base
+
 (require pollen/decode pollen/misc/tutorial txexpr)
+(require racket/string)
+
 (provide (all-defined-out))
 (define (root . elements)
    (txexpr 'root empty (decode-elements elements
      #:txexpr-elements-proc decode-paragraphs
-     #:string-proc (compose1 smart-dashes))))
+     #:string-proc (compose1 smart-dashes ellipses de-indexify punctuation-spacing))))
 
 (define (link url . xs)
   `(a ((href ,url)) ,@xs))
+
+(define (ellipses x)
+  (string-replace x "..." "…"))
+
+(define (de-indexify x)
+  (string-replace x "/index.html" ""))
+
+(define (punctuation-spacing x)
+  (string-replace x ":" ":"))
