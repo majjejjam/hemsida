c7f1a6570d8f2ca38eeda61e6f22f72649a33491
Miró Allard <miro.allard@pm.me>
2023-02-28 17:57:57 +0100

    Added link tag

--- pollen.rkt
+++ pollen.rkt
@@ -1,7 +1,10 @@
 #lang racket/base
 (require pollen/decode pollen/misc/tutorial txexpr)
-(provide root)
+(provide (all-defined-out))
 (define (root . elements)
    (txexpr 'root empty (decode-elements elements
      #:txexpr-elements-proc decode-paragraphs
      #:string-proc (compose1 smart-dashes))))
+
+(define (link url . xs)
+  `(a ((href ,url)) ,@xs))
