79656a2f915644a93baf9ab36ce5ad80c0351ad5
Miró Allard <miro.allard@pm.me>
2023-03-09 13:37:19 +0100

    Cleand up pollen.rkt

--- pollen.rkt
+++ pollen.rkt
@@ -4,19 +4,30 @@
 (require racket/string)
 
 (provide (all-defined-out))
-(define (root . elements)
-   (txexpr 'root empty (decode-elements elements
-     #:txexpr-elements-proc decode-paragraphs
-     #:string-proc (compose1 smart-dashes ellipses de-indexify punctuation-spacing))))
 
+;; Tags
 (define (link url . xs)
   `(a ((href ,url)) ,@xs))
 
+;; String processing
 (define (ellipses x)
   (string-replace x "..." "…"))
+
+(define (en-dashes x)
+  (string-replace x "--" "–"))
 
 (define (de-indexify x)
   (string-replace x "/index.html" ""))
 
 (define (punctuation-spacing x)
   (string-replace x ":" ":"))
+
+(define string-proc (compose1 ellipses
+                              en-dashes
+                              de-indexify
+                              punctuation-spacing))
+
+(define (root . elements)
+   (txexpr 'root empty (decode-elements elements
+     #:txexpr-elements-proc decode-paragraphs
+     #:string-proc string-proc)))
