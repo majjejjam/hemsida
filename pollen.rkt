a0b293aee5dfffcd8fbde2fb6c5062e01f2c09b1
Miró Allard <miro.allard@pm.me>
2023-03-17 15:11:48 +0100

    Added microtypographic styling function and node->link

--- pollen.rkt
+++ pollen.rkt
@@ -1,7 +1,8 @@
 #lang racket/base
 
 (require pollen/decode pollen/misc/tutorial txexpr)
-(require racket/string)
+(require racket/string racket/list)
+(require pollen/tag pollen/template pollen/core sugar/coerce)
 
 (provide (all-defined-out))
 
@@ -19,13 +20,36 @@
 (define (de-indexify x)
   (string-replace x "/index.html" ""))
 
-(define (punctuation-spacing x)
-  (string-replace x ":" ":"))
+(define (punctuation-spacing str)
+  (define substrs (regexp-match* #px"\\s?[:;()?]" str #:gap-select? #t))
+  (if (= (length substrs) 1)
+      (car substrs)
+      (cons 'quo (append-map (λ(str)
+                               (let ([strlen (string-length str)])
+                                 (if (> strlen 0)
+                                     (case (substring str (sub1 strlen) strlen)
+                                       [(":") (list '(squo-push) `(squo-pull ,str))]
+                                       [(";") (list '(squo-push) `(squo-pull ,str))]
+                                       [("?") (list '(squo-push) `(squo-pull ,str))]
+                                       [(")") (list '(squo-push) `(squo-pull ,str))]
+                                       [("(") (list '(dquo-push) `(dquo-pull ,str))]
+                                       [else (list str)])
+                                     (list str)))) substrs))))
 
-(define string-proc (compose1 ellipses
+(define string-proc (compose1 punctuation-spacing
                               en-dashes
-                              de-indexify
-                              punctuation-spacing))
+                              ellipses
+                              de-indexify))
+
+(define (node->link node)
+  (define node-string (->string node))
+  (define link-name (or (select-from-metas 'title node) node-string))
+  (link (string-append "/" node-string) link-name))
+
+(define (node->date node)
+  (define node-string (->string node))
+  (define date (or (select-from-metas 'date node) node-string))
+  (link (string-append "/" node-string) link-name))
 
 (define (root . elements)
    (txexpr 'root empty (decode-elements elements
