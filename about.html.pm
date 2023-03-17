90fab0b26df43fe9a9e431a2b7fbf792614bdaf4
Miró Allard <miro.allard@pm.me>
2023-03-17 16:12:27 +0100

    Updated about page with more information

--- about.html.pm
+++ about.html.pm
@@ -1,3 +1,24 @@
 #lang pollen
 
-Den här hemsidan är skapad med ◊a[#:href "https://docs.racket-lang.org/pollen/"]{Pollen}. Källkoden är tillgänglig på ◊a[#:href "https://github.com/"]{Github}.
+◊article{
+◊h1{Om hemsidan}
+
+Denna hemsida är skapad med ren ◊abbr{HTML} och ◊abbr{CSS}, samt ett språk vid namn ◊a[#:href "https://docs.racket-lang.org/pollen/"]{Pollen}. Källkoden finns tillgänglig på ◊a[#:href "https://github.com/"]{Github} under licensen ◊link["https://mit-license.org/"]{◊abbr{MIT}}.
+
+Om du upptäcker några fel, språkliga som programartade, välkomnar jag dig att ◊link["https://github.com/majjejjam/web/issues"]{öppna ett ärende}. På samma sätt kan du även föerlslå andra förbättringar och idéer. Viktigt att förstå är att hela hemsidan, med allt dess innehåll och kod, är ◊link["https://www.gnu.org/philosophy/free-sw.html"]{fri mjukvara}, vilket tillåter dig att
+
+◊ol[#:start "0"]{
+◊li{Navigera och konsumera det den har att erbjuda}
+◊li{Studera ◊link["https://github.com/majjejjam/web"]{hur den är uppbyggd}}
+◊li{Kopiera den i sin helhet och använda den för eget bruk (med undantag stjäla min identitet)}
+◊li{◊link["https://github.com/majjejjam/web/pulls"]{Bidra med din egen expertis} så att besökare får en bättre upplevelse}
+}
+
+Alla typsnitt som används är licensierade under ◊abbr{sil} Open Font License (◊abbr{ofl}):
+◊ul{
+◊li{◊link["https://vollkorn-typeface.com/"]{Vollkorn av ◊abbr{Friedrich Althausen}}}
+◊li{◊link["https://www.huertatipografica.com/en/fonts/alegreya-sans-ht"]{◊span[#:style "font-family: AlegreyaSans; font-size: 105%;"]{Alegreya Sans} av ◊abbr{Juan Pablo del Peral}}}
+◊li{◊link["https://fontlibrary.org/en/font/fantasque-sans-mono"]{◊span[#:style "font-family: Fantasque; opacity: 0.9"]{Fantasque} av ◊abbr{Jany Belluz}}}
+◊li{◊link["https://github.com/zeynepakay/Rakkas"]{◊span[#:style "font-family: Rakkas; opacity: 0.8;"]{Rakkas} av ◊abbr{Zeynep Akay}}}
+}
+}
