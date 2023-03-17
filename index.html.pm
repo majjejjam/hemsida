9d5aef9234a2a0b49f9e03d5b74768cb92142b3f
Miró Allard <miro.allard@pm.me>
2023-03-17 15:23:21 +0100

    Added latest posts to startpage

--- index.html.pm
+++ index.html.pm
@@ -1,3 +1,11 @@
 #lang pollen
+◊(require pollen/pagetree pollen/template sugar/coerce)
 
-Hej?
+◊h1[#:class "site-identity"]{Miró◊br{}Allard}
+
+◊(let () (current-pagetree (load-pagetree "index.ptree")) "")
+
+◊h2{Inlägg}
+◊(apply ul #:class "posts"
+(for/list ([post (children 'inlagg/index.html)])
+(cons 'li (list (node->link post) (div #:class "post-date" (or (select-from-metas 'date post) "20"))))))
