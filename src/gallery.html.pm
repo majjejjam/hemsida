9014a0b1214c793d622d7b5cb5557ef7e6b787f2
Miró Allard <miro.allard@pm.me>
2023-03-18 11:31:12 +0100

    Moved source files to src dir

---
+++ src/gallery.html.pm
@@ -0,0 +1,9 @@
+#lang pollen
+
+◊article{
+◊h1{Serier & bilder}
+Här kommer återfinnas serier, bilder etc. Allt utan JavaScript -- bara hederliga ◊abbr{html} och ◊abbr{css}.
+
+◊h2{Underrubrik här under}
+Med lite text som följer?
+}
