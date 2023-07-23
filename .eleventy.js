c069a29db579a4f8984416f26ef1e67f4910b35a
Miró Allard <miro.allard@pm.me>
2023-07-23 16:24:20 +0200

    Add render plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,7 @@
 const pluginWebc = require("@11ty/eleventy-plugin-webc");
+const { EleventyRenderPlugin } = require("@11ty/eleventy")
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPlugin(pluginWebc);
+    eleventyConfig.addPlugin(EleventyRenderPlugin);
 }
