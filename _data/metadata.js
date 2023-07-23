2ba18e3250934e03ed2dd3545f62101939f75dc0
Miró Allard <miro.allard@pm.me>
2023-07-23 18:59:26 +0200

    Add site metadata

---
+++ _data/metadata.js
@@ -0,0 +1,10 @@
+module.exports = {
+	title: "Miró Allards hemsida",
+	url: "https://miroallard.com/",
+	language: "sv",
+	description: "Här finns det jag önskar.",
+	author: {
+		name: "Miró Lieberman Allard",
+		url: "https://miroallard.com/om/#mig"
+	}
+}
