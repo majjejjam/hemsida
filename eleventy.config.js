a4279f92981524d18b6ce371b09896bb1cd78d07
Miró Allard <miro.allard@pm.me>
2024-03-05 13:36:02 +0100

    automatically use typographic characters

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,6 @@
 import pluginWebc from "@11ty/eleventy-plugin-webc";
 import pluginRss from "@11ty/eleventy-plugin-rss";
+import markdownIt from "markdown-it";
 
 export default function(eleventyConfig) {
 		eleventyConfig.addPassthroughCopy("bundle.css");
@@ -9,6 +10,15 @@ export default function(eleventyConfig) {
 		    components: ['src/_components/**/*.webc'],
 	  });
 		eleventyConfig.addPlugin(pluginRss);
+
+    let options = {
+        html: true,
+        breaks: false,
+        typographer: true,
+        quotes: '»«’’'
+    };
+
+    eleventyConfig.setLibrary("md", markdownIt(options));
 
 		return {
 				dir: {
